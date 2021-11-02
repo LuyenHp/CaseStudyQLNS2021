321795e1a0061f2a8970df3ea586019c875d45ff
leluye <leluyen1205@gmail.com>
2021-11-02 11:36:22 +0700

    Cập nhật 2112021

--- script.js
+++ script.js
@@ -4,25 +4,35 @@ function nhapData() {
   let DiaChi = document.getElementById("DiaChi").value;
   let NgaySinh = document.getElementById("NgaySinh").value;
   let GioiTinh = document.getElementById("GioiTinh").value;
+  if (HoTen.length < 2) {
+    document.getElementById("iferrorFullName").innerHTML =
+      "Tên của bạn quá ngắn!";
+  } else {
+    document.getElementById("iferrorFullName").innerHTML = "";
+  }
+
   if (
-    (HoTen != "") &
-    (Email != "") &
-    (DiaChi != "") &
-    (NgaySinh != "") &
-    (GioiTinh != "")
+    HoTen.length > 2 &&
+    Email.length > 10 &&
+    DiaChi.length > 0 &&
+    GioiTinh.length > 0
   ) {
-    // alert(HoTen + Email + DiaChi + NgaySinh + GioiTinh);
     let thongTinSv = [HoTen, Email, DiaChi, NgaySinh, GioiTinh];
     alert("Thong tin sinh vien: " + thongTinSv);
     taoMoi();
   }
 }
+
 function taoMoi() {
-  // document.getElementById("taoMoi").style.backgroundColor = "blue";
   let x = document.getElementById("formSinhVien");
+  let y = document.getElementById("dataTable");
   if (x.style.display == "block") {
     x.style.display = "none";
+    y.style.display = "block";
+    document.getElementById("taoMoi").innerHTML = "Tạo mới";
   } else {
     x.style.display = "block";
+    y.style.display = "none";
+    document.getElementById("taoMoi").innerHTML = "Xem bảng";
   }
 }
